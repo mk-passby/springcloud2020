11ad452fcc9ae96ffc4b408679bc152a22966adb
随机路人甲 <33092250@qq.com>
2020-08-03 23:12:56 +0800

    springcloud-gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/bli/guigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.bli.guigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: springcloud2020
+ * @description:
+ * @author: mk_passby
+ * @create: 2020-08-03 22:34
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/bli/guigu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.bli.guigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @program: springcloud2020
+ * @description:文档路径：https://cloud.spring.io/spring-cloud-static/spring-cloud-gateway/2.2.1.RELEASE/reference/html/
+ * @author: mk_passby
+ * @create: 2020-08-03 23:01
+ **/
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        //http://news.baidu.com/guoji
+        return builder.routes()
+            .route("path_route_bliguigu",
+                r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+    }
+}
